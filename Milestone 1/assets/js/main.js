e26de9b6874210490543e0cb4de6ef8657e17630
PierMario <piermario.salis@aol.com>
2020-12-21 11:26:53 +0100

    Update #2
    Icone e nomi utente, input bar ricerca nuova chat

--- Milestone 1/assets/js/main.js
+++ Milestone 1/assets/js/main.js
@@ -1,4 +1,29 @@
 let app = new Vue ({
     el: "#root",
-    
+
+    data: {
+
+        userAccount: {
+            avatar: './assets/img/avatar_io.jpg',
+            name: 'Nome Utente'
+        },
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: './assets/img/avatar_1.jpg'
+            },
+            {
+                name: 'Fabio',
+                avatar: './assets/img/avatar_2.jpg'
+            },
+            {
+                name: 'Samuele',
+                avatar: './assets/img/avatar_3.jpg'
+            },
+            {
+                name: 'Luca',
+                avatar: './assets/img/avatar_4.jpg'
+            }
+        ],
+    }
 })
