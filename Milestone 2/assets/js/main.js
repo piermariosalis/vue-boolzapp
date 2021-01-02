fd958844ebb5500a1bb3e313defc71e1c45bdb8f
PierMario <piermario.salis@aol.com>
2021-01-02 12:18:37 +0100

    Milestone 2
    Aggiunti messaggi contatti

---
+++ Milestone 2/assets/js/main.js
@@ -0,0 +1,52 @@
+let app = new Vue ({
+    el: "#root",
+
+    data: {
+
+        userAccount: {
+            avatar: './assets/img/avatar_io.jpg',
+            name: 'Nome Utente'
+        },
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: './assets/img/avatar_1.jpg',
+                messages: [
+                {
+                  date: '10/01/2020 15:30:55',
+                  text: 'Hai portato a spasso il cane?',
+                  status: 'sent'
+                },
+                {
+                  date: '10/01/2020 15:50:00',
+                  text: 'Ricordati di dargli da mangiare',
+                  status: 'sent'
+                },
+                {
+                date: '10/01/2020 16:15:22',
+                text: 'Tutto fatto!',
+                status: 'received'
+                }
+              ],
+            },
+            {
+                name: 'Fabio',
+                avatar: './assets/img/avatar_2.jpg'
+            },
+            {
+                name: 'Samuele',
+                avatar: './assets/img/avatar_3.jpg'
+            },
+            {
+                name: 'Luca',
+                avatar: './assets/img/avatar_4.jpg'
+            }
+        ],
+        mainUser : {
+            name: 'Michele',
+            lastSeen: 'Ultimo accesso oggi alle',
+            avatar: './assets/img/avatar_1.jpg'
+        },
+
+    }
+})
